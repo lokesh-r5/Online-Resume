0454a1ffb1483434f27df0ab13295cbce4b74779
Lokesh Reddyvari <lokeshreddyvari5@gmail.com>
2017-08-07 17:11:50 -0700

    Added data objects for the Resume

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,5 +1,103 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
+ var bio={
+   "name": "Lokesh Reddyvari",
+   "role": "Software Engineer",
+   "contacts": {
+     "mobile": "(669) 258-6855",
+     "email": "lokeshreddyvari5@gmail.com",
+     "github": "https://github.com/lokesh-reddyvari",
+     "twitter": "https://twitter.com/Im_loke",
+     "location": "San Jose, CA"
+   },
+   "welcomeMessage": "",
+   "skills": [],
+   "biopic": "",
+   "display": function(){
 
- 
+   }
+ };
+
+ var education= {
+   "schools":[{
+     "name": "San Jose State University",
+     "location": "San Jose",
+     "degree": "Master of Science",
+     "majors": ["Computer Engineering"],
+     "dates": "2015 - 2017",
+     "url": "http://www.sjsu.edu/"
+    },
+    {
+     "name": "Jawaharlal Nehru Technological University",
+     "location": "India",
+     "degree": "Bachelor of Science",
+     "majors": ["Computer Engineering"],
+     "dates": "2009 - 2013",
+     "url": "http://jntua.ac.in/"
+   }],
+   "onlineCourses": [{
+     "title": "Front End Developer",
+     "school": "Udacity",
+     "dates": "2017 - 2017",
+     "url": "udacity.com"
+   },
+   {
+     "title": "Front End Developer",
+     "school": "Free Code Camp",
+     "dates": "2017 - 2017",
+     "url": "freecodecamp.com"
+   }],
+   "display": function(){
+
+   }
+ };
+
+ var work={
+   "jobs": [{
+     "employer": "Tech Mahindra",
+     "title": "Software Engineer",
+     "location": "Hyderabad, India",
+     "dates": "2013 - 2015",
+     "description": ""
+   }],
+   "display": function(){
+
+   }
+ };
+
+ var projects={
+   "projects": [{
+     "title": "eHealth Web Application",
+     "dates": "2016-2017",
+     "description": "",
+     "images": []
+   },
+   {
+     "title": "Reading List Web Application",
+     "dates": "2017-2017",
+     "description": "",
+     "images": []
+   },
+   {
+     "title": "Steel Plant Database",
+     "dates": "2015-2015",
+     "description": "",
+     "images": []
+   },
+   {
+     "title": "CPU Design",
+     "dates": "2016-2017",
+     "description": "",
+     "images": []
+   },
+   {
+     "title": "Video Streaming Server",
+     "dates": "2015-2016",
+     "description": "",
+     "images": []
+   }],
+   display: function(){
+
+   }
+ };
